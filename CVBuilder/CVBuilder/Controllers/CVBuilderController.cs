017eb485a7bc08867fbefe933acaa6b9610b8584
INMihaylov19 <INMihaylov19@codingburgas.bg>
2023-07-13 13:26:53 +0300

    Start implementation of CV builder service

---
+++ CVBuilder/CVBuilder/Controllers/CVBuilderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVBuilder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CVBuilderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateCV()
+        {
+            // Create a new CV
+            return Ok();
+        }
+    }
+}
